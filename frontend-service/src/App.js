f4dbee082daec0886c670747bf42e7d8d8066566
Virtover <comodore9636@gmail.com>
2023-06-07 14:51:41 +0200

    xdd

--- frontend-service/src/App.js
+++ frontend-service/src/App.js
@@ -1,14 +1,35 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import React, { useState, useEffect } from 'react';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Chat from './components/Chat';
+import { LS_LOGIN_DATA } from './names';
 
-function App() {
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  useEffect(() => {
+    const loginData = localStorage.getItem(LS_LOGIN_DATA);
+    if (loginData) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  const toggleForm = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      {loggedIn ? (
+        <Chat />
+      ) : isSignIn ? (
+        <SignIn toggleForm={toggleForm} setLoggedIn={setLoggedIn} />
+      ) : (
+        <SignUp toggleForm={toggleForm} setLoggedIn={setLoggedIn} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
