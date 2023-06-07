4e9c14efdbe4a4b7b2412c75f048f5b63b393c50
Virtover <comodore9636@gmail.com>
2023-06-07 13:05:41 +0200

    gh

---
+++ frontend-service/src/components/Home.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../config";
+
+function Home() {
+  
+  return (
+    <>
+      <h1>Hellow world!</h1>
+    </>
+  );
+}
+
+export default Home;
