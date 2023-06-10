7b71baf2cac1b627074d478823a14d6ffa556a69
Virtover <comodore9636@gmail.com>
2023-06-10 15:16:35 +0200

    gyh

---
+++ frontend-service/src/components/utils/ChatMessagesContainer.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useRef } from 'react';
+import '../styles/chat.css';
+
+const ChatMessagesContainer = () => {
+    //TODO
+    return (<></>);
+}
+
+export default ChatMessagesContainer;
