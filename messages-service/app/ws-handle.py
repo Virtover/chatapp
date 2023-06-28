2267175dda3bb5bd8adedcf018f3f0eea4c0f625
Antoni Grodowski <53835371+AnthonyGrod@users.noreply.github.com>
2023-06-28 22:52:49 +0200

    Update ws-handle.py

--- messages-service/app/ws-handle.py
+++ messages-service/app/ws-handle.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import WebSocket
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -7,22 +7,34 @@ app = FastAPI()
 class ConnectionManager:
     def __init__(self):
         self.connections: List[WebSocket] = []
+        self.messages: List[str] = []  # Store the messages here
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.connections.append(websocket)
 
-    async def broadcast(self, data: str):
+        # Send the current messages to the connected client
+        await websocket.send_json(self.messages)
+
+    async def broadcast(self, message: str):
+        self.messages.append(message)  # Add the new message to the list
+
         for connection in self.connections:
-            await connection.send_text(data)
+            await connection.send_json(self.messages)  # Send the updated messages to all clients
 
 
 manager = ConnectionManager()
 
-# Websocket endpoint for user which concurrently sends and receives messages
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+# WebSocket endpoint for user which concurrently sends and receives messages
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
     await manager.connect(websocket)
     while True:
         data = await websocket.receive_text()
-        await manager.broadcast(f"Client {client_id}: {data}")
+        await manager.broadcast(data)  # Broadcast the received message to all clients
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8888)
